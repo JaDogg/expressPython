6c6c1422f7440c8af6e67ec3e4e50c24efab65fa
JaDogg <jadogg.coder@gmail.com>
2015-01-24 09:42:26 +0530

    Add comments to main.cpp

--- main.cpp
+++ main.cpp
@@ -4,13 +4,22 @@
 #include "UI/mainview.h"
 #include <QApplication>
 
+//snippet storage is static cause
+//it it should be only created once
 static Snippets *snip;
+
+//main view is static so it can be shared 
+//and only created once
 static MainView *mainView;
+
 int main(int argc, char *argv[]) {
+  
+  //need to set program details 
   QCoreApplication::setOrganizationName("Bhathiya");
   QCoreApplication::setOrganizationDomain("simpll.info");
   QCoreApplication::setApplicationName("expressPython");
 
+  //non modified arguments must be passed
   QApplication app(argc, argv);
 
   wchar_t name[] = L"expressPython";
