cbc5c49eda95ce1e9087eec03eae64f09fcceabd
Harshit Verma <hverma1@cs.iitr.ac.in>
2020-05-12 20:25:10 +0530

    feat(ep_jedi.py): added jedi unix support

--- ep_jedi.py
+++ ep_jedi.py
@@ -14,7 +14,16 @@ import subprocess
 def get_completions(source, row, col, script_path=""):
     return []
 
-
+def real(source, row, col, script_path=""):
+    completions = []
+    try:
+        script = jedi.Script(source, row, col+1, script_path)
+        completion_objects = script.complete()
+        completions = [x.name for x in completion_objects]
+    except Exception as err:
+        pass
+    finally:
+        return completions
 # WHY:
 # Only supports windows for now, we have full control cause we bundle everything!
 if os.name == "nt":
@@ -31,14 +40,15 @@ if os.name == "nt":
         python_location = os.path.join(d(d(d(d(jedi.__file__)))), "python.exe")
         if os.path.exists(python_location):
             sys.executable = python_location
+            get_completions = real
 
-            def real(source, row, col, script_path=""):
-                completions = []
-                try:
-                    script = jedi.Script(source, row + 1, col, script_path)
-                    completion_objects = script.completions()
-                    completions = [x.name for x in completion_objects]
-                finally:
-                    return completions
-
+if os.name == "posix":
+    try:
+        import jedi
+    except ImportError:
+        pass
+    else:
+        d = os.path.dirname
+        python_location = os.path.join(d(d(d(d(jedi.__file__)))), "python3.8")
+        if os.path.exists(python_location):
             get_completions = real
